147ad33a7dd4d35523ac777361881c5fa9e80e1c
Boby Subagja <boby.subagja@wgs-id.com>
2022-08-22 13:33:47 +0700

    Deploy the website into gh-pages

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -43,9 +43,9 @@ const Portfolio = () => {
 
               <span className="work__category">{category}</span>
               <h3 className="work__title">{title}</h3>
-              <a href="#" className=" work__button">
+              {/* <a href="#" className=" work__button">
                 <i className="icon-link work__button-icon"></i>
-              </a>
+              </a> */}
             </div>
           );
         })}
